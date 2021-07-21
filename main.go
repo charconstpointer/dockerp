950fcfa3bf816f8666103ae511b5dc1d0e7eef03
charconstpointer <attwistingnether@gmail.com>
2021-07-21 20:31:30 +0200

    init

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+)
+
+var (
+	ds = flag.String("ds", "", "ds server ip")
+)
+
+func main() {
+	flag.Parse()
+	if *ds == "" {
+		log.Fatal("ds server ip is empty")
+	}
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		log.Fatal(err)
+	}
+	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, c := range containers {
+		log.Println(c.Status)
+		log.Println(c.Ports[0].PublicPort)
+		go forward(fmt.Sprintf("%d", c.Ports[0].PublicPort), *ds)
+	}
+	<-ctx.Done()
+}
+
+func forward(port, dsAddr string) error {
+	log.Println("forward: ", fmt.Sprintf("%s:%s", dsAddr, port))
+	l, err := net.Listen("tcp", fmt.Sprintf("%s:%s", "0.0.0.0", port))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	conn, err := l.Accept()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	ds, err := net.Dial("tcp", fmt.Sprintf("%s:%s", dsAddr, port))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	go io.Copy(conn, ds)
+	_, err = io.Copy(ds, conn)
+	return err
+}
